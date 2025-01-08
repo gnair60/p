27e83b69ada207e70de4b250f560b4c40509b948
gnair60 <119920811+gnair60@users.noreply.github.com>
2025-01-08 00:09:50 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,6 @@ const Footer = () => {
 	return (
 		<div className='bg-slate-900 p-4' style={{ cursor: 'default' }}>
 			<div className='flex justify-between flex-wrap gap-4'>
-				<p className='text-white text-center w-full sm:w-auto font-light'>© {year} Gautam. All rights reserved.</p>
 				<div className='text-white flex justify-around sm:w-[250px] w-full'>
 					
 					<a href="mailto:gautamnair60@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
